b6d501854f977860731f13cb29053f8dd03d17c5
Theodore <pei.jiam@northeastern.com>
2025-01-21 19:29:04 +0000

    Initial commit: Created Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Calculator!");
+        // Main program logic will be added later
+    }
+}
