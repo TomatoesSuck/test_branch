e3946b25dbe086932a931a9684d533444b4f46c8
Theodore <pei.jiam@northeastern.com>
2025-01-21 19:31:53 +0000

    Added addition functionality

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,10 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to Calculator!");
         // Main program logic will be added later
+        System.out.println("Addition of 2 and 3 is: " + add(2, 3));
+    }
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
+
